6988769750de76bcd1531af7f9dcb7046b152529
Vincent van der Linden <vincent.van.der.linden@alliander.com>
2022-12-11 00:02:19 +0100

    betterer

--- 2022/q6.R
+++ 2022/q6.R
@@ -2,13 +2,13 @@ str <- readr::read_file("q6.in")
 input <- strsplit(str,"")[[1]]  
 
 #Part 1----
-  characters <- c()
-  loop_counter <- 1
 #Add all characters to a vector. If the unique tail (ooooh, I'm a unicorn!) equals 4, jump out of the loop
 get_marker <- function(input, no_of_unique_chars){
+  characters <- c()
+  loop_counter <- 1
   for(char in input){
     characters <- c(characters, char)
-    if(length(unique(tail(characters, no_of_unique_chars))) == no_of_unique_chars) return(loop_counter)
+    if (length(unique(tail(characters, no_of_unique_chars))) == no_of_unique_chars) return(loop_counter)
     loop_counter <- loop_counter + 1
   }
 }

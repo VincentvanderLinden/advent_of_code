e0a17f456a01d9ca077b731c9cae564ec92687c6
Vincent van der Linden <vincent.van.der.linden@alliander.com>
2022-12-11 18:18:23 +0100

    Time to eat some lamb chops!

--- 2022/q07.R
+++ 2022/q07.R
@@ -11,33 +11,35 @@ get_file_structure <- function(commands) {
   current_path <- "root/"
   
   for (command in commands) {
+    if (command == "$ cd /") next
     #Keep track of current directory
     if (grepl("\\$ cd ", command)) {
       #Get the path
       dir_name <- sub("\\$ cd ", "", command)
       if (dir_name == "..") {
         #Remove the last dir
-        current_path <- sub("\\w/$", "", current_path)
+        current_path <- sub("(?<=/)\\w*/$", "", current_path, perl = T)
       } else {
         current_path <- paste0(current_path, dir_name, "/")
       }
     }
-    
-    #Pick up command lines starting with a digit
+    #Pick up command lines starting with a digit as these are files
     if (grepl("^\\d+", command)) {
       #Get the file sizes (delete everything after the space)
       file_size <- as.numeric(sub(" .*", "", command))
       #Add file size to current_path
-      file_structure <- rbind(file_structure, data.frame(path = current_path, 
-                                                         size = file_size))
+      file_structure <- rbind(file_structure, 
+                              data.frame(path = current_path, 
+                                         size = file_size))
       #Add records to above directories if we are in a sub directory
       #Check depth
       if (stringr::str_count(current_path, "/") - 1 > 0){
         #Create paths for all above directories (parent paths) of current_path
         for (position in head(str_locate_all(current_path, "/")[[1]][, 1], - 1)) {
           #Add file size to parent paths
-          file_structure <- rbind(file_structure, data.frame(path = substr(current_path, 1, position), 
-                                                             size = file_size))
+          file_structure <- rbind(file_structure, 
+                                  data.frame(path = substr(current_path, 1, position), 
+                                             size = file_size))
         }
       }
     }
@@ -46,13 +48,34 @@ get_file_structure <- function(commands) {
 }
 #Part 1----
 #Answer:
-
-tmp <- get_file_structure(commands)
-tmp %>%
+get_file_structure(commands) %>%
   group_by(path) %>%
   summarise(size = sum(size)) %>%
   filter(size <= 1e5) %>%
   summarise(size = sum(size))
 
-#1182909
-#1003136
+#Part 2----
+path_structure <- 
+  get_file_structure(commands) %>%
+  group_by(path) %>%
+  summarise(size = sum(size)) 
+
+#Calculate space to free up
+total_space <- 7e7
+needed_space <- 3e7
+space_to_free_up <- 
+  abs(
+    total_space - 
+    path_structure %>%
+      filter(path == "root/") %>%
+      pull(size) - 
+    needed_space)
+
+#Answer:
+get_file_structure(commands) %>%
+  group_by(path) %>%
+  summarise(size = sum(size)) %>%
+  filter(size >= space_to_free_up) %>%
+  arrange(size) %>%
+  top_n(-1) %>%
+  select(size)

6b980077dad0cda44d89f1e48644e752fb2ecf4c
VincentvanderLinden <78534285+VincentvanderLinden@users.noreply.github.com>
2023-01-13 16:43:04 +0100

    Create q01.py

---
+++ 2022/Python/q01.py
@@ -0,0 +1,14 @@
+#Open file and split on double returns
+with open('q01.in') as f:
+    input = f.read().split("\n\n")
+#Split the lines into calories per elf
+input = [str.split(line, "\n") for line in input]
+#Convert to integers
+input = [[int(calories) for calories in elf] for elf in input]
+#Get the sums of calories by traversing the elf list
+elfs = [*map(sum, input)]
+#ANSWER 1: 
+max(elfs)
+#ANSWER 2: 
+elfs.sort()
+sum(elfs[-3:])

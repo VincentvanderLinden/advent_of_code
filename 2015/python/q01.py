e0c56451a9dc133c4809bf6f5fe14f5c79229731
Vincent van der Linden <vlinden@gmail.com>
2023-12-01 17:12:12 +0000

    2015

--- 2015/python/q01.py
+++ 2015/python/q01.py
@@ -5,3 +5,12 @@ with open('q01.in') as f:
 print(f"Answer 1: {txt.count('(') - txt.count(')')}")
 
 # Part 2
+floor = 0
+
+for ix, char in enumerate(txt): 
+    if char == '(': 
+        floor += 1
+    else: floor -= 1
+    if floor < 0: 
+        print(f"Answer 2: {ix + 1}")
+        break

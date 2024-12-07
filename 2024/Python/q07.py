c306b22eb846961552cb4583f17c81b17b7ea11f
VincentvanderLinden <78534285+VincentvanderLinden@users.noreply.github.com>
2024-12-07 10:21:45 +0100

    Create q07.py

---
+++ 2024/Python/q07.py
@@ -0,0 +1,34 @@
+with open('q07.in') as f: 
+    inp = [line.strip() for line in f.readlines()]
+
+R=0
+for line in inp: 
+    result = int(line.split(':')[0])
+    items = [int(l) for l in line.split(':')[1].split()]
+    res = [items[0]]
+    for i in range(1, len(items)): 
+        for j in res[-1*(2**i):]:
+            res.append(j + items[i])
+            res.append(j * items[i])
+            
+    if result in res[-1*(2**i):]: 
+        R+=result
+
+print(f"Part 1: {R}")
+
+
+# Brute forcing in batches             
+R = 0
+for line in inp[800:]: 
+    result = int(line.split(':')[0])
+    items = [int(l) for l in line.split(':')[1].split()]
+    res = [items[0]]
+    for i in range(1, len(items)): 
+        for j in res[-1*(3**i):]:
+            res.append(j + items[i])
+            res.append(j * items[i])
+            res.append(int(str(j) + str(items[i])))
+
+    if result in res[-1*(3**i):]: 
+        R+=result
+print(R)

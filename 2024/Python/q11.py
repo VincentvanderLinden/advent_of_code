d05f7163ff48160fdacda46c213c573f38d8b402
vincentvanderlinden <vlinden@gmail.com>
2024-12-12 12:18:18 +0100

    q11

--- 2024/Python/q11.py
+++ 2024/Python/q11.py
@@ -1,32 +1,43 @@
-with open('q11.in') as f: 
-    inp = [n for n in f.readline().split()]
+from collections import Counter
+from functools import cache
 
-def count_even_lengths(lst: list) -> int: 
-    return sum([1 for n in lst if len(n)%2 == 0])
+with open('q11.in') as f: 
+    inp = [int(n) for n in f.readline().split()]
 
 #print(count_even_lengths(inp))    
 YEAR = 2024
 
-def blink(lst: list) -> list:
-    new_list = []
-    for e in lst: 
-        if e == '0':
-            new_list.append('1')
-        elif len(e)%2 == 0: 
-            new_list += [str(int(e[:len(e)//2])), str(int(e[len(e)//2:]))]
-        else: 
-            new_list.append(str(int(e) * YEAR))
-    return new_list
-        
-for n in range(20): 
+
+@cache
+def process_value(val: int) -> list:
+    if val == 0:
+        result = [1] 
+    elif len(str(val)) % 2 == 0:
+        mid = len(str(val)) // 2
+        left_half = int(str(val)[:mid])
+        right_half = int(str(val)[mid:])
+        result = [left_half, right_half] 
+    else:
+        result = [val * YEAR] 
     
-    if n == 0: 
-        prev_l = len(inp)
-        l = blink(inp)
-        new_l = len(l)
-    else: 
-        prev_l = len(l)
-        l = blink(l)
-        new_l = len(l)
-    print(f"{prev_l=}, {new_l=}, {new_l - prev_l=}")
-print(f"Answer 1: {len(l)}")
+    return result 
+        
+@cache
+def fast_blink(stone: int, blinks: int) -> int:
+    if blinks == 0:
+        return 1
+    
+    transformed_stones = process_value(stone)
+    return sum(fast_blink(num, blinks - 1) for num in transformed_stones)
+
+def run(inp, times): 
+    total = 0 
+    for stone in inp: 
+        total += fast_blink(stone, times)
+    return total
+        
+print(f"Answer 1: {run(inp, 25)}")
+print(f"Answer 2: {run(inp, 75)}")
+
+
+

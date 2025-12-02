1c0e941c40d0914d1219251f722332ad94a46c88
vincentvanderlinden <vlinden@gmail.com>
2025-12-02 16:17:27 +0100

    2025

---
+++ 2025/q01.py
@@ -0,0 +1,36 @@
+with open('q01.in') as f: 
+    input = f.read().splitlines()
+    
+pos = 50
+answer1 = 0
+
+for line in input: 
+    dir = line[0]
+    n = int(line[1:])
+
+    s = n if dir == 'R' else -n
+
+    pos = (pos + s) % 100    
+
+    if pos == 0: 
+        answer1 += 1
+    
+print(f"Answer 1: {answer1}")
+
+with open('q01.in') as f: 
+    input = f.read().splitlines()
+
+answer2 = 0
+pos = 50
+
+for line in input: 
+    dir = line[0]
+    n = int(line[1:])
+    
+    if dir == 'L':
+            answer2 += ((100-pos)%100 + n)//100
+            n *= -1
+    else:
+        answer2 += (pos+n)//100
+    pos = (pos+n)%100
+print(f"Answer 2: {answer2}")
---
+++ 2025/q02.py
@@ -0,0 +1,27 @@
+import re
+
+with open('q02.in') as f: 
+    input = f.read().split(',')
+    
+answer1 = 0
+answer2 = 0
+
+
+for line in input: 
+    a, b = map(int, line.split('-'))
+    for i in range(a, b+1): 
+        j = str(i)
+        if j[:len(j)//2] == j[len(j)//2:]: 
+            answer1 += i
+            
+        # Part 2
+        match = re.match(r"^([0-9]+?)\1+$", j)
+        if  match: 
+            answer2 += i
+            # print(i, match.groups(), match.span())
+            next
+
+print(f"Answer 1: {answer1}")
+print(f"Answer 2: {answer2}")
+
+
